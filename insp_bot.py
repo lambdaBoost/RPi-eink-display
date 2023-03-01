e0dc5485a0ee24ef12193e6891ca5b18a3017f40
Alex Hall <analytics@alex-hall.co.uk>
2023-03-01 21:58:57 +0000

    deleted redundant file

--- insp_bot.py
+++
@@ -1,9 +0,0 @@
-import requests
-from io import BytesIO
-from PIL import Image
-
-r = requests.get("https://inspirobot.me/api?generate=true")
-im_url = r.content.decode()
-
-img = Image.open(requests.get(im_url, stream=True).raw)
-img = img.save("img1.jpg")
